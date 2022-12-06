7e609c4024e8b6933c885ab4883240378e52d9b8
Stanislav <rus.abaaac@gmail.com>
2022-12-06 11:57:07 +0600

    deployed first NFT

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config({ path: ".env" });
+
+const API_ALCHEMY = process.env.API_ALCHEMY;
+const PRIV_KEY = process.env.PRIV_KEY;
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/" + API_ALCHEMY,
+      accounts: [PRIV_KEY]
+    }
+  }
+};
